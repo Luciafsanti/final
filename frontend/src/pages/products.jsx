18d56c7ff40239db5151f9711dfca4de5cb9d82d
Luciafsanti <luciafsanti@gmail.com>
2024-05-10 00:17:29 -0300

    gestionar productos principal

--- frontend/src/pages/products.jsx
+++ frontend/src/pages/products.jsx
@@ -1,157 +1,65 @@
-import FormContainer from "../components/form/formContainer";
-import FormComponent from "../components/form/form";
-import FormInput from "../components/form/formInput";
-import FormTitle from "../components/form/formTitle";
-import FormButton from "../components/form/formButton";
-import { useState } from "react";
-import axios from "axios";
-import ErrorSpan from "../components/form/error-span";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 import useBooks from "../hooks/use-books";
+import Table from "../components/table";
+import styled from "styled-components";
+import BodyContainer from "../components/body-container";
+import FormTitle from "../components/form/formTitle";
 
-const Products = () => {
-  const [title, setTitle] = useState("");
-  const [author, setAuthor] = useState("");
-  const [ISBN, setISBN] = useState("");
-  const [price, setPrice] = useState(0);
-  const [stock, setStock] = useState(0);
-  const [categoryId, setCategoryId] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
-  const [description, setDescription] = useState("");
-  const [errors, setErrors] = useState({});
-  const navigate = useNavigate();
+const Container = styled.div`
+  width: 80vw;
+  gap: 1rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+function Products() {
   const { books } = useBooks();
 
-  console.log(
-    title,
-    author,
-    ISBN,
-    price,
-    stock,
-    categoryId,
-    imageUrl,
-    description,
-    errors
-  );
-
-  console.log(
-    !title,
-    !author,
-    !price,
-    !ISBN,
-    !stock,
-    !categoryId,
-    !imageUrl,
-    !description
-  );
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    let errorFinded = false;
-    if (
-      !title ||
-      !author ||
-      !price ||
-      !ISBN ||
-      !stock ||
-      !categoryId ||
-      !imageUrl ||
-      !description ||
-      books.find((book) => book.ISBN === ISBN)
-    ) {
-      setErrors((prev) => ({
-        ...prev,
-        newBook: {
-          message: books.find((book) => book.ISBN === ISBN)
-            ? "Este libro ya existe."
-            : "Son requeridos todos los campos",
-        },
-      }));
-      errorFinded = true;
-    } else {
-      setErrors((prev) => ({
-        ...prev,
-        username: null,
-      }));
-    }
-
-    if (errorFinded) {
-      return;
-    }
-
-    axios
-      .post("http://localhost:3000/libros", {
-        title: title,
-        author: author,
-        ISBN: ISBN,
-        price: price,
-        stock: stock,
-        category_id: categoryId,
-        image_url: imageUrl,
-        description: description,
-      })
-      .then(function (response) {
-        console.log(response);
-        navigate("/");
-        alert("Producto añadido exitosamente");
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
-
   return (
-    <FormContainer>
-      <FormTitle>
-        <h2>Registrarse</h2>
-      </FormTitle>
-      <FormComponent onSubmit={handleSubmit} action="/">
-        {errors.newBook && <ErrorSpan>{errors.newBook.message}</ErrorSpan>}
-        <FormInput
-          type="text"
-          placeholder="Titulo"
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <FormInput
-          type="text"
-          placeholder="Autor"
-          onChange={(e) => setAuthor(e.target.value)}
-        />
-        <FormInput
-          type="text"
-          placeholder="ISBN"
-          onChange={(e) => setISBN(e.target.value)}
-        />
-        <FormInput
-          type="number"
-          placeholder="Precio"
-          onChange={(e) => setPrice(parseFloat(e.target.value))}
-        />
-        <FormInput
-          type="number"
-          placeholder="Stock"
-          onChange={(e) => setStock(e.target.value)}
-        />
-        <FormInput
-          type="number"
-          placeholder="ID de categoria"
-          onChange={(e) => setCategoryId(e.target.value)}
-        />
-        <FormInput
-          type="text"
-          placeholder="Imagen de portada"
-          onChange={(e) => setImageUrl(e.target.value)}
-        />
-        <FormInput
-          type="text"
-          placeholder="Descripción"
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <FormButton>Registrarse</FormButton>
-      </FormComponent>
-    </FormContainer>
+    <BodyContainer>
+      <Container>
+        <FormTitle>
+          <h2>Gestionar Productos</h2>
+        </FormTitle>
+        <Table style={{ maxWidht: "80vw" }}>
+          <thead>
+            <tr>
+              <th>Titulo</th>
+              <th>Autor</th>
+              <th>ISBN</th>
+              <th>Precio</th>
+              <th>Stock</th>
+              <th>Categoria</th>
+              <th>Imagen</th>
+              <th>Descripcion</th>
+            </tr>
+          </thead>
+          <tbody>
+            {books.map((book) => {
+              return (
+                <tr>
+                  <td>{book.title}</td>
+                  <td>{book.author}</td>
+                  <td>{book.ISBN}</td>
+                  <td>{book.price}</td>
+                  <td>{book.stock}</td>
+                  <td>{book.category_id}</td>
+                  <td>
+                    <img
+                      src={book.image_url}
+                      alt=""
+                      style={{ width: "5rem" }}
+                    />
+                  </td>
+                  <td>{book.description}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </Container>
+    </BodyContainer>
   );
-};
+}
 
 export default Products;
